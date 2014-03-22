59ece2d21fa4976aaebf3c492b6e45b5469d5259
John Moore <john@programiscellaneous.com>
2014-03-22 03:52:07 -0400

    removed unnecessary imports

--- EC504_Project/src/SocketClient.java
+++ EC504_Project/src/SocketClient.java
@@ -1,13 +1,6 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.sql.Date;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
