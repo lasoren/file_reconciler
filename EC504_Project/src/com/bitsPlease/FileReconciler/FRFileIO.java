64bfa0126a0c1aabb23c994b4455a6d2815f83ef
Alexandrea Mellen <asmellen@terrapincomputing.com>
2014-03-22 09:11:11 -0400

    FRFileIO

---
+++ EC504_Project/src/com/bitsPlease/FileReconciler/FRFileIO.java
@@ -0,0 +1,77 @@
+package com.bitsPlease.FileReconciler;
+
+import java.util.*;
+import java.io.*;
+
+
+public class FRFileIO {
+	
+    public static void main(String[] args) {
+    	byte [] f = readIn("FIRST.txt");
+    	writeOut(f, "final.txt");
+    }
+    
+    public static byte[] readIn(String textFile){
+    	/*
+    	 * FileChannel fc = new FileInputStream(file).getChannel();
+         MappedByteBuffer mbb = fc.map(FileChannel.MapMode.READ_ONLY, 0, (int) fc.size());
+    	 */
+    	
+        File file = new File(textFile);
+        // List<byte[]> bArray = new ArrayList<byte[]>();
+        byte[] b = new byte[(int) file.length()];
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            fileInputStream.read(b);
+            //for (int i = 0; i < b.length; i++) {
+            // System.out.print((char)b[i]);
+            
+            
+            //}
+            fileInputStream.close();
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found.");
+            e.printStackTrace();
+        }
+        catch (IOException e1) {
+            System.out.println("Error Reading The File.");
+            e1.printStackTrace();
+        }
+        return b;
+    }
+    
+    public static void writeOut(byte[] b, String textFile){
+        //try {
+        
+		//String decoded = new String(b, "ASCII");
+		try {
+	        File file = new File(textFile);
+			FileOutputStream fileOutputStream = new FileOutputStream(file);
+			try {
+				fileOutputStream.write(b);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				fileOutputStream.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//PrintWriter writer = new PrintWriter("final.txt", "ASCII");
+			//for(int i = 0; i < b.length; i++){
+            //System.out.println(new String(b, "ASCII"));
+            //writer.print(new String(b, "ASCII"));
+			//}
+			//writer.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        
+    }
+}
