3a7d7d9a69956dd46f2c10d1514b16a18deff85c
John Moore <john@programiscellaneous.com>
2014-04-23 01:01:55 -0400

    fix

--- EC504_Project/src/com/bitsPlease/FileReconciler/CommandLine.java
+++ EC504_Project/src/com/bitsPlease/FileReconciler/CommandLine.java
@@ -9,7 +9,7 @@ public class CommandLine {
 	static String ip;
 	static String also;
 
-	public static void check(String[] args) {
+	public static void check(String[] args) throws Exception {
 
 		Options opt = new Options();
 		opt.addOption("file", true, "the file name");
@@ -22,19 +22,23 @@ public class CommandLine {
 			if(line.hasOption("file")) {
 				fileName = line.getOptionValue("file");
 
-			} else System.out.println("Include the file name after the parameter -file");
+			} else {
+				System.out.println("Include the file name after the parameter -file");
+				throw new Exception("Did not receive file argument");
+			}
 			
 			if (line.hasOption("to")){
 				ip = line.getOptionValue("to");
-			} else System.out.println("Include the IP of the recepient after the parameter -to");
+			} else {
+				System.out.println("Include the IP of the recepient after the parameter -to");
+				throw new Exception("Did not receive host argument");
+			}
 			
 			if (line.hasOption("also")) {
 				also = line.getOptionValue("also");
-			}
-			if (ip == null) {
-				System.out.println("Reconcile file " + fileName + " with localhost");
+				System.out.println("Reconcile file " + fileName + " with " + ip + " and " + also);
 			} else {
-				System.out.println("Reconcile file " + fileName + " with computer " + ip);
+				System.out.println("Reconcile file " + fileName + " with " + ip);
 			}
 		} catch (org.apache.commons.cli.ParseException exp) {
 			System.out.println("Invalid expression: " + exp.getMessage());
